6a6b2da21bae9977f36bb212e8eba2ab545c0eac
medovgeo <ellaud314@gmail.com>
2020-06-07 12:48:23 +0300

    adds base and main pages

---
+++ pages/base_page.py
@@ -0,0 +1,9 @@
+class BasePage(object):
+    pass
+
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
